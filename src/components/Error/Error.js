012ef6aa96d33b09b3b29c9dcd158e26574cfd60
Joel Mäenpää <zoloppe4lyf@gmail.com>
2019-04-03 11:37:01 +0300

    forgot to save

--- src/components/Error/Error.js
+++ src/components/Error/Error.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-class SignInPage extends Component {
+class Error extends Component {
     render() {
         return (
             <div>
@@ -10,4 +10,4 @@ class SignInPage extends Component {
     }
 }
 
-export default SignInPage;
+export default Error;
