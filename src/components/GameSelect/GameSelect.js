251d64df0a05e9908cad54d10afd68ffcc28b82c
Joel Mäenpää <zoloppe4lyf@gmail.com>
2019-04-11 09:36:58 +0300

    yeya

--- src/components/GameSelect/GameSelect.js
+++ src/components/GameSelect/GameSelect.js
@@ -5,7 +5,7 @@ class GameSelect extends Component {
     constructor() {
         super()
         this.state = {
-
+            
         }
     }
 
@@ -13,6 +13,7 @@ class GameSelect extends Component {
         return (
             <div className="select-wrapper">
                 <div className="button-group">
+                    <h1>Game Select</h1>
                     <button className="select-button">Classic</button>
                     <button className="select-button">Spree</button>
                     <button className="select-button">Clash</button>
