b59d985afc590b928ee8444affe6634f77e23993
Eetu <eetu.heikkinen12@gmail.com>
2019-04-15 10:42:29 +0300

    Login and register. Removed some duplicated files.

---
+++ MadMind/app/Http/Controllers/profileController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class profileController extends Controller
+{
+    /**
+     * Update the user's profile.
+     *
+     * @param  Request  $request
+     * @return Response
+     */
+    public function update(Request $request)
+    {
+        if (Auth::check()) {
+            // The user is logged in...
+        }
+        // $request->user() returns an instance of the authenticated user...
+    }
+    public function __construct()
+    {
+    $this->middleware('auth');
+    $this->middleware('auth:api');
+    }
+    /**
+     * Get the path the user should be redirected to.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return string
+     */
+    protected function redirectTo($request)
+    {
+        return route('login');
+    }
+
+}
