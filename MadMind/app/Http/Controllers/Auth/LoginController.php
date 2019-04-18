a98a096b6a9df938e4e4e5ce79eb36e206a49294
Eetu <eetu.heikkinen12@gmail.com>
2019-04-18 10:59:32 +0300

    Fixing controller names again.

---
+++ MadMind/app/Http/Controllers/Auth/LoginController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Foundation\Auth\AuthenticatesUsers;
+
+class loginController extends Controller
+{
+    /*
+    |--------------------------------------------------------------------------
+    | Login Controller
+    |--------------------------------------------------------------------------
+    |
+    | This controller handles authenticating users for the application and
+    | redirecting them to your home screen. The controller uses a trait
+    | to conveniently provide its functionality to your applications.
+    |
+    */
+
+    use AuthenticatesUsers;
+
+    /**
+     * Where to redirect users after login.
+     *
+     * @var string
+     */
+    protected $redirectTo = '/home';
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('guest')->except('logout');
+    }
+    public function username()
+    {
+        return 'username';
+    }
+ 
+    public function authenticate(Request $request)
+    {
+        $credentials = $request->only('username', 'password');
+
+        if (Auth::attempt($credentials)) {
+            // Authentication passed...
+            return redirect()->intended('dashboard');
+        }
+        if (Auth::attempt(['username' => $username, 'password' => $password, 'active' => 1])) {
+            // The user is active, not suspended, and exists.
+        }
+        if (Auth::guard('admin')->attempt($credentials)) {
+            //
+        }
+        if (Auth::attempt(['username' => $username, 'password' => $password], $remember)) {
+            // The user is being remembered...
+        }
+        Auth::logout();
+    }
+}
+
+//derp
