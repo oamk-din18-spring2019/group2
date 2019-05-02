9d6990800f9dacd090d2f03a179d38d20c44123f
Riku Heikkinen <riku.heikkinen94@gmail.com>
2019-05-02 10:06:35 +0300

    build number 254

---
+++ react/build/precache-manifest.d431d7b86341dfb03897278660ddfc5e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b020daee86e997921684aa5732dd27dc",
+    "url": "/static/media/madmindNoText.b020daee.png"
+  },
+  {
+    "revision": "e886468ac1f44dfeeba6b36a4fbbebd1",
+    "url": "/static/media/madmind.e886468a.png"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "40718b0f5bad42d26cb9",
+    "url": "/static/js/main.5447722b.chunk.js"
+  },
+  {
+    "revision": "bb54edf14e6c08d46d2e",
+    "url": "/static/js/2.2f61013d.chunk.js"
+  },
+  {
+    "revision": "40718b0f5bad42d26cb9",
+    "url": "/static/css/main.fbef20f7.chunk.css"
+  },
+  {
+    "revision": "1b573d0c431f65af928b1a318471d84a",
+    "url": "/index.html"
+  }
+];
